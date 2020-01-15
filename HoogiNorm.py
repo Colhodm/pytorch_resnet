016ecec31c130b3d5f2676b03c2b76d1cb54667b
Colhodm <arjunmishra@berkeley.edu>
2020-01-14 16:00:43 -0800

    Debugging stuff

--- HoogiNorm.py
+++ HoogiNorm.py
@@ -38,12 +38,6 @@ def group_norm(input, group, running_mean, running_var, weight=None, bias=None,
         if running_var is not None:
             running_var_orig = running_var
             running_var = running_var_orig.repeat(b)
-
-        # norm_shape = [1, b * c / group, group]
-        # print(norm_shape)
-        # Apply instance norm
-        # input_reshaped_1 = input.contiguous().view(1, int(b * c/group), group, *input.size()[2:])
-        # input_reshaped = input_reshaped_1
         eps = 1e-5
         G = group
         input_mod = input
@@ -51,111 +45,57 @@ def group_norm(input, group, running_mean, running_var, weight=None, bias=None,
         input_cpu = input.to('cpu')
         x = input_cpu.to('cpu')
         x_np = x.detach().numpy()
-        # x_np = x
+        # At this point we have just stored our input on CPU with Numpy
+        print(33333,x_np,x_np.shape)
         Nabs, C, H, W = x_np.shape
         N = Nabs
-        # x_np_tmp = x.detach().numpy()
+        # N refers to the __? What the batch size?
+        # res_x is the original shape of our input
         res_x = np.zeros((N, C, H, W))
+        # this is a looser form where we are just by channel dimensions
         x_np_new = np.zeros((C, N * H * W))
-        # tmpp = np.zeros((N, H*W))
+        # we loop through the channels
         for i in range(C):
+            # we are converting the raw input data such that there are channel rows of large N*H*W blocks
+            # why?
             x_np_new[i, :] = np.reshape(x_np[:, i, :, :], (1, N * H * W))
-        # x_np_new = np.reshape(x_np, (C, N*H*W))
-        # x_np = x_np.transpose()
+        # 60 by 10000 trying to cluster in this basis
+        # we create a copy of our reformatted data
         image_vector = np.asarray(x_np_new)
         Data = data_preparation(n_cluster=G, data=image_vector[:, :])
+        # Up to here we are all correct, as long as we are clustering in the right basis
         count = 0
         Common_mul = 1
         Common_add = 0
-        '''for val in range(G):
-            if (len(np.argwhere(Data.labels_ == val)) == 0):
-                pass
-            else:
-                Common_mul *= len(np.argwhere(Data.labels_ == val))
-                Common_add += len(np.argwhere(Data.labels_ == val))
-                #print(Common)
-        input_mod = np.zeros((input.shape[0], Common_mul * Common_add, input.shape[2], input.shape[3]))
-        input_mod = torch.from_numpy(input_mod).float().to('cuda')
-        for val in range(G):
-            inx = np.argwhere(Data.labels_ == val)
-            for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
-                    pass
-                else:
-                    #for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    for d in range(Common_mul):
-                        input_mod[:, d + count, :, :] = input[:, idxx[0], :, :]
-            #count += len(inx)
-            count += Common_mul'''
         tmp1 = torch.zeros(())
         for val in range(G):
+            # this creates all a list of all the indices where our label is valid 
             inx = np.argwhere(Data.labels_ == val)
-            if (len(np.argwhere(Data.labels_ == val)) > 0):
-                tmp = torch.zeros((input.shape[0], len(np.argwhere(Data.labels_ == val)), input.shape[2], input.shape[3]))
+            if len(inx) > 0:
+                tmp = torch.zeros((input.shape[0], len(inx), input.shape[2], input.shape[3]))
             for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
+                if len(inx) == 0:
                     pass
                 else:
-                    # for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    # for d in range(Common_mul):
+                    print(idx,idxx)
+                    # This is line of code is effectively saying fill into our temp channel equal to
+                    # the original index from input for all the matching indices. so we have temp has 
+                    # number of channels assigned to that cluster
                     tmp[:, idx, :, :] = input[:, idxx[0], :, :]
+                    # now we have the correct grouping so we choose to normalize
+            # STAT is the function which I think has a bug...
             out_final_tmp = (Stat_torch(tmp)).float().to('cuda')
             for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
+                if (len(inx) == 0):
                     pass
                 else:
-                    # for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    # for d in range(Common_mul):
+                    # here we assign that the the original index of out_final is equal to the temp
+                    # index it was given in the normalized group
                     out_final[:, idxx[0], :, :] = out_final_tmp[:, idx, :, :]
-        '''tmp1 = np.zeros(())
-        for val in range(G):
-            inx = np.argwhere(Data.labels_ == val)
-            if (len(np.argwhere(Data.labels_ == val)) > 0):
-                tmp = np.zeros((input.shape[0], len(np.argwhere(Data.labels_ == val)), input.shape[2], input.shape[3]))
-            for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
-                    pass
-                else:
-                    # for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    # for d in range(Common_mul):
-                    tmp[:, idx, :, :] = x_np[:, idxx[0], :, :]
-            out_final_tmp = torch.from_numpy(Stat(tmp)).float().to('cuda')
-            for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
-                    pass
-                else:
-                    # for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    # for d in range(Common_mul):
-                    out_final[:, idxx[0], :, :] = out_final_tmp[:, idx, :, :]'''
-
-            # count += Common_mul
-        # input_reshaped = input_mod.contiguous().view(1, int(b * (Common_mul * Common_add)/group), group, *input.size()[2:])
-        # input_reshaped = input_mod.contiguous().view(1, int(b * (Common_mul * Common_add)), group,
-        # *input.size()[2:])
-
-        # out = HGN(input_mod, group, Data)
-        # out = F.batch_norm(
-        # input_reshaped, running_mean, running_var, weight=weight, bias=bias,
-        #   training=use_input_stats, momentum=momentum, eps=eps)
-        '''for val in range(G):
-            inx = np.argwhere(Data.labels_ == val)
-            for idx, idxx in enumerate(inx):
-                if (len(np.argwhere(Data.labels_ == val)) == 0):
-                    pass
-                else:
-                    #for d in range(int(Common / len(np.argwhere(Data.labels_ == val)))):
-                    for d in range(Common_mul):
-                        out_final[:, idxx[0], :, :] = out[:, d + count, :, :]
-            #count += len(inx)
-            count += Common_mul
-        # Reshape back
-        if running_mean is not None:
-            running_mean_orig.copy_(running_mean.view(b, int(c/group)).mean(0, keepdim=False))
-        if running_var is not None:
-            running_var_orig.copy_(running_var.view(b, int(c/group)).mean(0, keepdim=False))'''
-        # print(out_final)
+        # we reshape it to the original size
+        # this is probably fine since code doesnt error...
+        # although double check this last line
         return out_final.view(b, c, *input.size()[2:])
-        # return
 
     return _instance_norm(input, group, running_mean=running_mean,
                           running_var=running_var, weight=weight, bias=bias,
@@ -181,8 +121,9 @@ class HoogiNorm(_BatchNorm):
             input, self.num_groups, self.running_mean, self.running_var, self.weight, self.bias,
             self.training or not self.track_running_stats, self.momentum, self.eps)
 
-
+# Deprecated Method of Normalization
 def Stat(IN):
+    print('I AM WORKING UP TO HERE')
     tmp = np.zeros((IN.shape[0]))
     tmp2 = 0
     eps = 1e-5
@@ -205,32 +146,18 @@ def Stat(IN):
     for i in range(IN.shape[0]):
         for j in range(IN.shape[1]):
             out[i, j, :, :] = (1 / sigma[i]) * (IN[i, j, :, :] - mu[i])
+    print(out)
     return out
 
-
+# Current Method of Normalization
 def Stat_torch(IN):
     tmp = torch.zeros((IN.shape[0]))
     tmp2 = 0
     eps = 1e-5
     sigma = torch.zeros((IN.shape[0], 1))
     out = IN
-    # print('IN is:')
-    # print(IN.shape)
-    '''for i in range(IN.shape[1]):
-        tmp += np.sum(IN[:,i,:,:])'''
     res = torch.zeros((IN.shape[0], 2, IN.shape[2], IN.shape[3]))
     res[:, 0, :, :], res[:, 1, :, :] = torch.std_mean(IN + eps, dim=1)
-    #sigma = torch.std
-    #for i in range(IN.shape[0]):
-        # for j in range(IN.shape[1]):
-        
-
-    # tmp = np.sum(IN, where=[False, True, True, True])
-    #mu = (1 / (IN.shape[1] * IN.shape[2] * IN.shape[3])) * tmp
-    '''for i in range(IN.shape[0]):
-        sigma[i] = np.sqrt(
-            (1 / (IN.shape[1] * IN.shape[2] * IN.shape[3])) * (np.sum((IN[i, :, :, :] - mu[i]) ** 2) + eps))'''
-    # sigma = np.sqrt((1 / (IN.shape[0] * IN.shape[2] * IN.shape[3])) * tmp2)
     for i in range(IN.shape[0]):
         for j in range(IN.shape[1]):
             out[i, j, :, :] = (1 / res[i, 0]) * (IN[i, j, :, :] - res[i, 1])
